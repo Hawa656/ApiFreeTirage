d692e2c5d96c5c2e80d573a17374502f54c7d809
Hawa656 <107039828+Hawa656@users.noreply.github.com>
2022-08-19 09:33:34 +0000

    &er commit

---
+++ FreeTirage/FreeTirage/src/test/java/com/odc/FreeTirage/FreeTirageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odc.FreeTirage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeTirageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

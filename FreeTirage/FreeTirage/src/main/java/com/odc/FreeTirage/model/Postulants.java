7384d6458231a0f3fbaa9ffc5cfb2344ff0a85ad
sbore669 <107039494+sbore669@users.noreply.github.com>
2022-08-19 12:05:14 +0000

    class postul tirage add

--- FreeTirage/FreeTirage/src/main/java/com/odc/FreeTirage/model/Postulants.java
+++ FreeTirage/FreeTirage/src/main/java/com/odc/FreeTirage/model/Postulants.java
@@ -1,4 +1,20 @@
 package com.odc.FreeTirage.model;
 
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class Postulants {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPostulants;
+    private String nom;
+    private String prenom;
+    private String numero;
+    private String email;
+
+
 }
--- FreeTirage/FreeTirage/src/main/java/com/odc/FreeTirage/model/Tirage.java
+++ FreeTirage/FreeTirage/src/main/java/com/odc/FreeTirage/model/Tirage.java
@@ -1,4 +1,20 @@
 package com.odc.FreeTirage.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
 public class Tirage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTirage;
+    private Date date;
+    private String libelle;
+
 }
